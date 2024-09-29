78819044d6733f14ff0b60ea5029e0624a694f29
Paul Wandimi <paulmuruthi@gmail.com>
2024-09-29 19:20:40 +0300

    add util fn to normalize sound

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import random
+import subprocess
+import os
 
 def pick_random_animal(file_path):
     selected_animal = None
@@ -13,3 +15,20 @@ def pick_random_animal(file_path):
                 selected_animal = line.strip()
 
     return selected_animal
+
+
+def normalize_sound(basedir, input_file, output_file):
+    input_file_path = os.path.join(basedir, input_file)
+    output_file_path = os.path.join(basedir, output_file)
+
+    ffmpeg_command = [
+        'ffmpeg',
+        '-i', input_file_path,
+        '-c:v', 'copy',
+        '-af', 'loudnorm=I=-14:TP=-2:LRA=11',
+        output_file_path
+    ]
+
+    subprocess.run(ffmpeg_command, capture_output=True)
+
+    os.remove(input_file_path)
