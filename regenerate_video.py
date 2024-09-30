64db76087b51030d383328125592602aab963046
Paul Wandimi <paulmuruthi@gmail.com>
2024-09-30 13:42:04 +0300

    enhance regenerate video script

--- regenerate_video.py
+++ regenerate_video.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python3
 
 import video
-import sys
 import os
 import json
+import argparse
+import utils
+import upload
 
 if __name__ == "__main__":
-    basedir = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--basedir", type=str, required=True)
+    parser.add_argument("--caption_settings", type=str, required=False)
+    args = parser.parse_args()
+
+    basedir = args.basedir
     narrations = [f for f in os.listdir(os.path.join(basedir, "narrations")) if f.endswith(".mp3")]
     caption_settings = {}
-    if len(sys.argv) > 2:
-        with open(sys.argv[2]) as f:
+    if args.caption_settings:
+        with open(args.caption_settings) as f:
             caption_settings = json.load(f)
-    video.create(narrations, basedir, output_filename="short.avi", caption_settings=caption_settings)
+
+    output_file = "short.avi"
+
+    video.create(narrations, basedir, output_filename=output_file, caption_settings=caption_settings)
+
+    print("Normalizing sound...")
+    normalized_output_file = f"normalized_{output_file}"
+    utils.normalize_sound(basedir, output_file, normalized_output_file)
+
+    print(f"DONE! Here's your generated video: {os.path.join(basedir, normalized_output_file)}")
+
+    print("Generating upload config...")
+    upload_config_file = utils.generate_upload_config(basedir)
+
+    print("Uploading video...")
+    youtube = upload.get_authenticated_service()
+    config = upload.load_config(upload_config_file)
+    if upload.upload_video(youtube, config) is True:
+        print(f"DONE! Uploaded video to YouTube")
+    else:
+        print(f"FAILED! Failed to upload video to YouTube")
